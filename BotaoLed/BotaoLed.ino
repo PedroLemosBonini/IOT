251713f3d3a6e4df216fe9a83a95d716df4ba0eb
pedrolemosbonini <pedrolemosbonini@gmail.com>
2025-09-08 11:18:41 -0300

    Alarme com ESP32

--- BotaoLed/BotaoLed.ino
+++ BotaoLed/BotaoLed.ino
@@ -1,41 +1,56 @@
 #include <math.h>
 #include "AdafruitIO_WiFi.h"
+#include "NewPing.h"
 
 
-// Configurações da rede WIFI
-#define WIFI_SSID "IIIII"
-#define WIFI_PASS "bonas231"
+#define BUZZER_PIN 27
+#define LED_ALARME 13
+#define BOTAO_FISICO 26
+#define TRIG_PIN 12
+#define ECHO_PIN 14
 
-// Autenticação Adafruit IO
-#define IO_USERNAME ""
-#define IO_KEY ""
+// Configuração do ultrassonico
+#define MAX_DISTANCE 100
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
 
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+// // Configurações da rede WIFI
+// #define WIFI_SSID "IIIII"
+// #define WIFI_PASS "bonas231"
 
-#define pinLed 14 //Pino do LED
+// // Autenticação Adafruit IO
+// #define IO_USERNAME ""
+// #define IO_KEY ""
 
-AdafruitIO_Feed *botaoled = io.feed("botaoled");
+// AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+ #define pinLed 14 //Pino do LED
+
+// AdafruitIO_Feed *botaoled = io.feed("botaoled");
 
 void setup() {
-  pinMode(pinLed, OUTPUT);
+  // pinMode(pinLed, OUTPUT);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_ALARME, OUTPUT);
+  pinMode(BOTAO_FISICO, INPUT);
 
   Serial.begin(115200);
 
   while(!Serial);
 
-  Serial.print("Conectando ao Adafruit IO");
-  io.connect();
+  // Serial.print("Conectando ao Adafruit IO");
+  // io.connect();
 
-  while(io.status() < AIO_CONNECTED){
-    Serial.print(".");
-    delay(500);
-  }
+  // while(io.status() < AIO_CONNECTED){
+  //   Serial.print(".");
+  //   delay(500);
+  // }
 
-  Serial.println();
-  Serial.println(io.statusText());
+  // Serial.println();
+  //Serial.println(io.statusText());
 
   // Configuração do callack, quando o feed receber(atualizar) um valor
-  botaoled -> onMessage(handleBotaoLed);
+  //botaoled -> onMessage(handleBotaoLed);
   // Registra a função de callback
   // Ela será chamada sempre que o feed receber um novo dado
 
@@ -43,12 +58,16 @@ void setup() {
 
 void loop() {
 
-  // Manter a conexão com o Adafruit IO ativa
-  io.run();
+  // // Manter a conexão com o Adafruit IO ativa
+  // io.run();
 
-  // Chamada da função publish
-  // publicacao();
+  // // Chamada da função publish
+  // // publicacao();
 
-  delay(3000);
+  // Serial.print(F("Distancia Lida: "));
+  // Serial.println(sonar.ping_cm());
+  // delay(500);
+
+  // delay(3000);
 
 }
